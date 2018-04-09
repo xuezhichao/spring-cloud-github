42dd9a5fd2f9e552aa1ebc13e47e20f40864ee18
xuezhichao <bin.bin.1022@163.com>
2018-04-09 10:31:03 +0800

    修改配置文件
    修改注册中心和客户端配置

--- eureka-client/src/main/java/com/lucky/eureka/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/lucky/eureka/EurekaClientApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class EurekaApplication {
+public class EurekaClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
+		SpringApplication.run(EurekaClientApplication.class, args);
 	}
 }
